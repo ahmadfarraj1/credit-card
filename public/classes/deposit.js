502d1a46f64dfda46068c90077e74238f7e9f3d6
ahmad farraj <67468632+ahmadfarraj1@users.noreply.github.com>
2021-01-30 14:34:35 +0200

    Add files via upload

---
+++ public/classes/deposit.js
@@ -0,0 +1,13 @@
+export class Deposit {
+    constructor(cardnumber, cardholder, amount, expdate, cardcvv) {
+        this.cardnumber = cardnumber;
+        this.cardholder = cardholder;
+        this.amount = amount;
+        this.expdate = expdate;
+        this.cardcvv = cardcvv;
+    }
+    ;
+    format() {
+        return ` Amount is $${this.amount} for ${this.cardholder}, Card Number ${this.cardnumber}, Expiration Date ${this.expdate}, CVV ${this.cardcvv}`;
+    }
+}
